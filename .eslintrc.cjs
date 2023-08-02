1723fb51ddfd1b522d6d7146dbd68feee094f519
antasth <antasth@mail.ru>
2023-08-02 23:03:35 +0300

    feat: add files

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'no-debugger': 'off',
+    'no-console': 0,
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/lines-between-class-members': 'off',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array',
+      },
+    ],
+    'max-lines-per-function': ['error', 40],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+  },
+};
