772a83d4443b92b42b5ee7a930d3365c34897934
antasth <antasth@mail.ru>
2023-08-05 10:04:49 +0300

    feat: add reactLogo

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,9 @@ function App(): JSX.Element {
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
